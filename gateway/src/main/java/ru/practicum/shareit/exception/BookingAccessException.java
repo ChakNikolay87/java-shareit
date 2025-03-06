d63e47951c1f6de9c4c6fb72c5874d5116262fcb
ChakNikolay87 <ChakNikolay@Yandex.ru>
2025-03-06 10:54:58 +0300

    Delete gateway directory

--- gateway/src/main/java/ru/practicum/shareit/exception/BookingAccessException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exception;
-
-public class BookingAccessException extends Exception {
-    public BookingAccessException(String message) {
-        super(message);
-    }
-}
