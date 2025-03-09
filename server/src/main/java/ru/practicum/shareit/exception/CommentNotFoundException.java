436e9d64ad238768227ab3217aa556d12221b308
ChakNikolay87 <ChakNikolay@Yandex.ru>
2025-03-09 11:19:51 +0300

    Delete server directory

--- server/src/main/java/ru/practicum/shareit/exception/CommentNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exception;
-
-public class CommentNotFoundException extends Exception {
-    public CommentNotFoundException(String message) {
-        super(message);
-    }
-}
